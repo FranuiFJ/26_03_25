2ec801a364f060330bb9149d3090e36491e6213c
FranuiFJ <francodiazvelez@itisjt.edu.ar>
2025-04-09 15:09:52 -0300

    -

---
+++ src/P3.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+#include "stm32f103xb.h"
+
+void setup() {
+  RCC->APB2ENR|=RCC_APB2ENR_IOPAEN_Msk;
+  GPIOB->CRH =0x44444444;
+  GPIOB->CRL =0x44444444;
+  GPIOA->CRH =0x11111111;
+  GPIOA->CRL =0x11111111;
+}
+
+void loop() {
+  uint16_t B =GPIOB->IDR;
+  GPIOA->BSRR =~B + 1u; 
+}
